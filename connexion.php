9b6c536238faca177234d84a3d46b7e4167d4401
STE301 <96244522+STE301@users.noreply.github.com>
2022-05-19 11:45:14 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,6 +1,5 @@
 <?php
 if(isset($_POST['user']) && isset($_POST['pass']) ){
-    session_start();
     $compte = $_POST['user'];
     $pass   = $_POST['pass'];  
 //paramètres d'accès au serveur base de données MySQL    
@@ -19,8 +18,7 @@ $sql = "SELECT user, passwd FROM comptes
 $res = mysqli_query($mysqli, $sql);
 $rowcount=mysqli_num_rows($res);
 if($rowcount==1){
-  $_SESSION['user']=$_POST['user'];
-  header('location:Vuebienvenue.php');
+  header('location:control.php');
 }else{
 header('location:index.php?reponse=\"Nom d\'utilisateur ou mot de passe non valide\"');
 }
