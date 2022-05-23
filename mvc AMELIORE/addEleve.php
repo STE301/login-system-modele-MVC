6080e683d06ced32898b5df5463368cc61a0143b
ikramFD <77616309+ikyyyyy@users.noreply.github.com>
2022-05-23 14:23:37 +0200

    Create addEleve.php

---
+++ mvc AMELIORE/addEleve.php
@@ -0,0 +1,51 @@
+<?php
+    require 'modele.php';
+try{
+    if( isset($_POST['signup']) && !empty($_FILES['picture'])) {
+
+        $cne = $_POST['cne'];
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $city = $_POST['city'];
+        $email = $_POST['email'];
+        $adress = $_POST['adresse'];
+        
+        
+        $fileName = $_FILES['picture']['name'];
+        $fileTmpName = $_FILES['picture']['tmp_name'];
+        $fileSize = $_FILES['picture']['size'];
+        $fileError = $_FILES['picture']['error'];
+        $fileType = $_FILES['picture']['type'];
+
+        $fileExtension = explode('.', $fileName);
+        $fileActualExtension = strtolower(end($fileExtension));
+        $allowedExtensions = array('jpeg', 'png', 'jpg', 'pdf');
+        if(in_array($fileActualExtension, $allowedExtensions)) {
+            if($fileError === 0){
+
+                if($fileSize < 100000){
+
+                    $fileNewName = uniqid('', true) . "." . $fileActualExtension;
+                    $fileDestination = 'uploads/' . $fileNewName;
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                    $res = addEleve($cne, $lastName, $firstName, $adress, $city, $email, $fileNewName, '0');
+
+                }else{
+                    echo 'your file size is too big !';
+                }
+                
+            }else{
+                echo 'there was an error uploading your file !';
+            }
+                
+        }else{
+            echo 'files of this type can\'t be uploaded!';
+        }
+    }
+
+}catch(Exception $except){
+    $msgErreur = $except->getMessage() ;
+    require 'vueErreur.php';
+}
+        
+?>
