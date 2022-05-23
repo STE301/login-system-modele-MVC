7269be29a4b65a288e4fa47f87889d9756e34e22
ikramFD <77616309+ikyyyyy@users.noreply.github.com>
2022-05-23 14:24:43 +0200

    Update modele.php

--- mvc AMELIORE/modele.php
+++ mvc AMELIORE/modele.php
@@ -6,6 +6,33 @@ function connectionDB(){
 
 function getEleve(){
     $bdd = connectionDB();
-    $resultat = $bdd->query('SELECT * FROM ELEVE');
+    $resultat = $bdd->query('SELECT * FROM ELEVES');
     return $resultat;
 }
+
+
+function addEleve($a, $b, $c, $d, $e, $f, $g, $h){
+try{
+    //add student
+    $sql = "INSERT INTO ELEVES (CNE, Nom, Prenom, Adresse, Ville, email, Photo, etat)  VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    $bdd = connectionDB();
+    $result = $bdd->prepare($sql);
+    
+    $result->bindParam(1, $cne);
+    $result->bindParam(2, $lastName);
+    $result->bindParam(3, $firstName);
+    $result->bindParam(4, $adress);
+    $result->bindParam(5, $city);
+    $result->bindParam(6, $email);
+    $result->bindParam(7, $fileNewName);
+    $result->bindParam(8, $etat);
+
+    $cne = $a; $lastName = $b; $firstName = $c; $adress = $d; $city = $e; $email = $f; $fileNewName = $g; $etat = $h;
+
+    $result->execute();
+
+    echo "New records created successfully";
+}catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+}               
