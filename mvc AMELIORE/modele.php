d26161ac41215bbf3fb8e80da58b88c010fcd53f
ikramFD <77616309+ikyyyyy@users.noreply.github.com>
2022-06-01 09:26:11 +0200

    Update modele.php

--- mvc AMELIORE/modele.php
+++ mvc AMELIORE/modele.php
@@ -1,38 +1,45 @@
 <?php
-function connectionDB(){
-    $bdd = new PDO('mysql:host=localhost;dbname=dbensat;charset=utf8','root', '',  array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));            
-    return $bdd;
-}
+    function connectionDB(){
+        $bdd = new PDO('mysql:host=localhost;dbname=ensat;charset=utf8','root', '',  array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));            
+        return $bdd;
+    }
 
-function getEleve(){
-    $bdd = connectionDB();
-    $resultat = $bdd->query('SELECT * FROM ELEVES');
-    return $resultat;
-}
+    function getStudents(){
+        $bdd = connectionDB();
+        $resultat = $bdd->query('SELECT * FROM ELEVES');
+        return $resultat;
+    }
 
 
-function addEleve($a, $b, $c, $d, $e, $f, $g, $h){
-try{
-    //add student
-    $sql = "INSERT INTO ELEVES (CNE, Nom, Prenom, Adresse, Ville, email, Photo, etat)  VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-    $bdd = connectionDB();
-    $result = $bdd->prepare($sql);
+    function addStudent($param1, $param2, $param3, $param4, $param5, $param6, $param7, $param8){
+        try{
+            //add student
+            $sql = "INSERT INTO ELEVES (CNE, Nom, Prenom, Adresse, Ville, email, Photo, etat)  VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            $bdd = connectionDB();
+            $result = $bdd->prepare($sql);
     
-    $result->bindParam(1, $cne);
-    $result->bindParam(2, $lastName);
-    $result->bindParam(3, $firstName);
-    $result->bindParam(4, $adress);
-    $result->bindParam(5, $city);
-    $result->bindParam(6, $email);
-    $result->bindParam(7, $fileNewName);
-    $result->bindParam(8, $etat);
+            $result->bindParam(1, $cne);
+            $result->bindParam(2, $lastName);
+            $result->bindParam(3, $firstName);
+            $result->bindParam(4, $adress);
+            $result->bindParam(5, $city);
+            $result->bindParam(6, $email);
+            $result->bindParam(7, $fileNewName);
+            $result->bindParam(8, $etat);
 
-    $cne = $a; $lastName = $b; $firstName = $c; $adress = $d; $city = $e; $email = $f; $fileNewName = $g; $etat = $h;
+            $cne = $param1; 
+            $lastName = $param2; 
+            $firstName = $param3; 
+            $adress = $param4; 
+            $city = $param5; 
+            $email = $param6; 
+            $fileNewName = $param7; 
+            $etat = $param8;
 
-    $result->execute();
+            $result->execute();
 
-    echo "New records created successfully";
-}catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-}               
+            echo "New records created successfully";
+        }catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }               
