bb1b05125fe1bebfd76f3d54423323264ad84e10
ikramFD <77616309+ikyyyyy@users.noreply.github.com>
2022-06-01 09:43:49 +0200

    Update controler.php

--- mvc AVANCE/CONTROLER/controler.php
+++ mvc AVANCE/CONTROLER/controler.php
@@ -1,1 +1,11 @@
+<?php
+    require 'MODEL/model.php';
 
+    function welcomePage(){
+        $list = getStudents();
+        require 'viewWelcome.php';
+    }
+
+    function errorPage($msgErreur){
+        require 'viewError.php';
+    }
