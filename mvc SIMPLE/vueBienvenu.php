3d836479a6b85facb16f96cd67ba567221d4c498
ikramFD <77616309+ikyyyyy@users.noreply.github.com>
2022-06-01 08:42:34 +0200

    Update vueBienvenu.php

--- mvc SIMPLE/vueBienvenu.php
+++ mvc SIMPLE/vueBienvenu.php
@@ -6,13 +6,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="designstyle.css">
+    <link rel="stylesheet" href="style.css">
     <title>Plateforme de l'ENSAT</title>
 </head>
 <body>
     <tbody>
     <h2 class="h12">Liste des étudiants</h2>
-    <h2 class="h12">****************</h2>
+    <hr>
     <br>
         <table class="table1">
             <tr class="tete">
@@ -25,16 +25,16 @@
                 <td>Photo d'identité</td>
                 <td>Etat</td>
             </tr>
-            <?php   foreach ($res1 as $res1){    ?>
+            <?php   foreach ($list as $row){    ?>
             <tr>
-                <td><?php echo $res1['CNE'];?></td>
-                <td><?php echo $res1['Nom'];?></td>
-                <td><?php echo $res1['Prenom'];?></td>
-                <td><?php echo $res1['Adresse'];?></td>
-                <td><?php echo $res1['Ville'];?></td>
-                <td><?php echo $res1['email'];?></td>
-                <td><img src="<?php echo $res1['Photo'];?>" width="100%"></td>
-                <td><?php echo $res1['etat'];?></td>
+                <td><?php echo $row['CNE'];?></td>
+                <td><?php echo $row['Nom'];?></td>
+                <td><?php echo $row['Prenom'];?></td>
+                <td><?php echo $row['Adresse'];?></td>
+                <td><?php echo $row['Ville'];?></td>
+                <td><?php echo $row['email'];?></td>
+                <td><img src="<?php echo $row['Photo'];?>" width="100%"></td>
+                <td><?php echo $row['etat'];?></td>
             </tr>
             <?php } ?>
         </table> 
