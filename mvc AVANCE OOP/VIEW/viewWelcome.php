f00687351e761a6723da25249db6afd1176e9093
ikramFD <77616309+ikyyyyy@users.noreply.github.com>
2022-06-12 13:27:02 +0200

    Create viewWelcome.php

---
+++ mvc AVANCE OOP/VIEW/viewWelcome.php
@@ -0,0 +1,33 @@
+<?php $this->title = 'Plateforme de L\'ENSAT'; ?>
+
+<?php ob_start(); ?>
+
+<table class="table1">
+            <tr class="tete">
+                <td>CNE</td>
+                <td>Nom</td>
+                <td>Prenom</td>
+                <td>Adresse</td>
+                <td>Ville</td>
+                <td>Email</td>
+                <td>Photo d'identité</td>
+                <td>Etat</td>
+            </tr>
+            <?php   foreach ($list as $row){    ?>
+            <tr>
+                <td><?= $row['CNE'];?></td>
+                <td><?= $row['Nom'];?></td>
+                <td><?= $row['Prenom'];?></td>
+                <td><?= $row['Adresse'];?></td>
+                <td><?= $row['Ville'];?></td>
+                <td><?= $row['email'];?></td>
+                <td><img src="<?= $row['Photo'];?>" width="100%"></td>
+                <td><?= $row['etat'];?></td>
+            </tr>
+            <?php } ?>
+</table> 
+
+<center><a href="http://localhost/PROJET_AMECHNOUE/MVC simple/index.php?action=vAddStudent"> <button>Ajouter Etudiant</button></a></center>  
+<center><a href="#"> <button>Deconnexion</button></a></center>  
+
+<?php $this->content = ob_get_clean(); ?>
