4805d254cd3d21deee189a7c836cfcfdafd05304
STE301 <96244522+STE301@users.noreply.github.com>
2022-06-12 18:22:17 +0100

    Delete mvc AMELIORE directory

--- mvc AMELIORE/viewError.php
+++
@@ -1,7 +0,0 @@
-<?php $titre = 'Plateforme de l\'ENSAT'; ?>
-<?php ob_start() ?>
-
-<p>Une erreur est survenue : <?= $msgErreur ?></p>
-
-<?php $contenu = ob_get_clean(); ?>
-<?php require 'template.php'; ?>
