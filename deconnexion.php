3d4c8e839f809d9db296364ad4d089180cbc9597
STE301 <96244522+STE301@users.noreply.github.com>
2022-06-12 15:58:53 +0100

    Delete deconnexion.php

--- deconnexion.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-if(isset($_SESSION['user'])){
-    session_destroy();
-    header('location:./index.html');} else{
-        header('location:allMembers.php?reponse=\"Violation d\'accés\"');
-    }
-?>
